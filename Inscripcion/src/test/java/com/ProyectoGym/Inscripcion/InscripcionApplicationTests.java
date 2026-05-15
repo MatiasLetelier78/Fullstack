a541e0d67e4832b2f22330c811558fc76e74c759
MatiasLetelier78 <leter1410@gmail.com>
2026-05-14 21:56:54 -0400

    Inscripcion

---
+++ Inscripcion/src/test/java/com/ProyectoGym/Inscripcion/InscripcionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoGym.Inscripcion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscripcionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
