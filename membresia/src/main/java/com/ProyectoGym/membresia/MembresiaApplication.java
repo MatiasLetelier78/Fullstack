703c5b4b27feef852732ee71f635936775cdcdf4
MatiasLetelier78 <leter1410@gmail.com>
2026-05-14 21:04:26 -0400

    Matias Letelier

---
+++ membresia/src/main/java/com/ProyectoGym/membresia/MembresiaApplication.java
@@ -0,0 +1,14 @@
+package com.ProyectoGym.membresia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembresiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembresiaApplication.class, args);
+		System.out.println("Hola");
+	}
+
+}
