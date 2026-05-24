86e5378aa53817f8a9b2397ae50fbc7332f9de10
MatiasLetelier78 <leter1410@gmail.com>
2026-05-24 19:55:41 -0400

    Commit Actualizado

--- Inscripcion/src/main/java/com/Gym/Inscripcion/InscripcionApplication.java
+++ Inscripcion/src/main/java/com/Gym/Inscripcion/InscripcionApplication.java
@@ -2,7 +2,8 @@ package com.Gym.Inscripcion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.openfeign.EnableFeignClients;
+@EnableFeignClients
 @SpringBootApplication
 public class InscripcionApplication {
 
