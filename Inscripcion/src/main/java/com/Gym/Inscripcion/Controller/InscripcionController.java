68710186929966d77d8b105cb5a04738ad3155c9
MatiasLetelier78 <leter1410@gmail.com>
2026-05-24 20:34:16 -0400

    Commit Actualizado

--- Inscripcion/src/main/java/com/Gym/Inscripcion/Controller/InscripcionController.java
+++ Inscripcion/src/main/java/com/Gym/Inscripcion/Controller/InscripcionController.java
@@ -1,4 +1,53 @@
 package com.Gym.Inscripcion.Controller;
 
+import com.Gym.Inscripcion.Model.Inscripcion;
+import com.Gym.Inscripcion.Service.InscripcionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/inscripciones")
 public class InscripcionController {
+
+    @Autowired
+    private InscripcionService inscripcionService;
+
+    @GetMapping("")
+    public List<Inscripcion> listarInscripciones() {
+        return inscripcionService.listarInscripciones();
+    }
+
+    @GetMapping("/id_clase/{idClase}")
+    public List<Inscripcion> buscarPorIdClase(@PathVariable String idClase) {
+        return inscripcionService.findByIdUsuario(idClase);
+    }
+
+    @GetMapping("/id_usuario/{idUsuario}")
+    public List<Inscripcion> buscarPorIdUsuario(@PathVariable String idUsuario) {
+        return inscripcionService.findByIdUsuario(idUsuario);
+    }
+
+    @PostMapping("/")
+    public Inscripcion agregarInscripcion(@RequestBody Inscripcion nueva) {
+        return inscripcionService.agregarInscripcion(nueva);
+    }
+
+    @DeleteMapping("/{idUsuario}/{idClase}")
+    public boolean eliminarInscripcion(
+            @PathVariable String idUsuario,
+            @PathVariable String idClase
+    ) {
+        return inscripcionService.deleteInscripcion(idUsuario, idClase);
+    }
+
+    @PutMapping("/{idUsuario}/{idClase}")
+    public Inscripcion actualizarInscripcion(
+            @PathVariable String idUsuario,
+            @PathVariable String idClase,
+            @RequestBody Inscripcion nueva
+    ) {
+        return inscripcionService.updateInscripcion(idUsuario, idClase, nueva);
+    }
 }
