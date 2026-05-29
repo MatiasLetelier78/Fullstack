f808ac1f8e74988b4c7cce6c0592464f7ce11b93
Mativs <m.franke.u@gmail.com>
2026-05-28 23:12:07 -0400

    Se incorporan los try catch y logs en el service de ms Asistencia

--- Asistencia/src/main/java/com/gym/Asistencia/Repository/AsistenciaRepository.java
+++ Asistencia/src/main/java/com/gym/Asistencia/Repository/AsistenciaRepository.java
@@ -1,7 +1,6 @@
 package com.gym.Asistencia.Repository;
 
 
-import com.gym.Asistencia.AsistenciaApplication;
 import com.gym.Asistencia.Model.Asistencia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
