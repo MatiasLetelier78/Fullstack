eefb28d839f470e1d686c60d7f41b942cd97fea9
MatiasLetelier78 <leter1410@gmail.com>
2026-05-28 20:09:50 -0400

    Pago Actualizacion

--- Pago/src/main/java/com/Gym/Pago/Service/PagoService.java
+++ Pago/src/main/java/com/Gym/Pago/Service/PagoService.java
@@ -24,9 +24,7 @@ public class PagoService {
 
     public List<Pago> findByIdUsuario(String idUsuario) { return pagoRepository.findByIdUsuario(idUsuario); }
 
-    public List<Pago> findByIdMembresia(String idMembresia){
-        return pagoRepository.findByIdMembresia(idMembresia);
-    }
+    public List<Pago> findByIdMembresia(String idMembresia){ return pagoRepository.findByIdMembresia(idMembresia); }
 
     public Pago agregarPago(Pago pago){
         return pagoRepository.save(pago);
@@ -46,10 +44,8 @@ public class PagoService {
     public Pago actualizarPagoByIdPago(Pago pago, String idPago){
         Pago p = pagoRepository.findByIdPago(idPago);
         if(p != null){
-
-            // ✅ Validación: la fecha de pago no puede ser futura
             if(pago.getFechaPago().isAfter(LocalDate.now())){
-                return null; // o puedes lanzar una excepción
+                return null;
             }
 
             p.setMontoPago(pago.getMontoPago());
