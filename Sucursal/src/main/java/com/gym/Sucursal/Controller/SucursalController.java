67a029a11d7f5311ef888872eab3e3a8aa4b96c0
Mativs <m.franke.u@gmail.com>
2026-06-21 17:59:57 -0400

    Se hace documentación de los microservicios Asistencia, Empleado, Sucursal y Usuario

--- Sucursal/src/main/java/com/gym/Sucursal/Controller/SucursalController.java
+++ Sucursal/src/main/java/com/gym/Sucursal/Controller/SucursalController.java
@@ -3,6 +3,11 @@ package com.gym.Sucursal.Controller;
 
 import com.gym.Sucursal.Model.Sucursal;
 import com.gym.Sucursal.Service.SucursalService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,12 +17,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping("/api/v1/sucursales")
+@Tag(name = "API Sucursal", description = "API para la gestion de sucursales")
 @RestController
 public class SucursalController {
 
     @Autowired
     private SucursalService sucursalService;
     @GetMapping("")
+    @Operation(summary = "Listar las sucursales",description = "Obtiene la lista de todas las sucursales disponibles")
+    @ApiResponse(responseCode = "200", description = "Sucursales encontradas de forma exitosa")
+    @ApiResponse(responseCode = "204", description = "No existen sucursales registradas en el sistema")
     public ResponseEntity<List<Sucursal>> getAllSucursales() {
         List<Sucursal> listado = sucursalService.listarSucursales();
         if (listado.isEmpty()) {
@@ -28,7 +37,10 @@ public class SucursalController {
     }
 
     @GetMapping("/{idSucursal}")
-    public ResponseEntity<Sucursal> getSucursalById(@PathVariable String idSucursal) {
+    @Operation(summary = "Buscar sucursal por ID", description = "Busca la sucursal disponible mediante su ID")
+    @ApiResponse(responseCode = "200", description = "Sucursal localizada correctamente")
+    @ApiResponse(responseCode = "404", description = "La sucursal con el ID especificado no existe")
+    public ResponseEntity<Sucursal> getSucursalById(@Parameter(description = "ID a buscar") @PathVariable String idSucursal) {
         Sucursal buscado = sucursalService.buscarSucursalPorCodigoSucursal(idSucursal);
         if (buscado != null) {
             return new ResponseEntity<>(buscado, HttpStatus.OK);
@@ -38,7 +50,10 @@ public class SucursalController {
     }
 
     @GetMapping("/nombre_sucursal/{nombre}")
-    public ResponseEntity<Sucursal> getSucursalByNombre(@PathVariable String nombre) {
+    @Operation(summary = "Buscar sucursal por NOMBRE", description = "Busca la sucursal disponible mediante su NOMBRE")
+    @ApiResponse(responseCode = "200", description = "Sucursal localizada correctamente")
+    @ApiResponse(responseCode = "404", description = "La sucursal con el NOMBRE especificado no existe")
+    public ResponseEntity<Sucursal> getSucursalByNombre(@Parameter(description = "NOMBRE a buscar")@PathVariable String nombre) {
         Sucursal buscado = sucursalService.buscarSucursalPorNombreSucursal(nombre);
         if (buscado != null) {
             return new ResponseEntity<>(buscado, HttpStatus.OK);
@@ -48,6 +63,9 @@ public class SucursalController {
     }
 
     @PostMapping("/")
+    @Operation(summary = "Crear nueva sucursal", description = "Crea una nueva sucursal en la base de datos")
+    @ApiResponse(responseCode = "201", description = "Sucursal creada exitosamente")
+    @ApiResponse(responseCode = "400", description = "Error al crear la sucursal")
     public ResponseEntity<Sucursal> createSucursal(@RequestBody @Valid Sucursal sucursal) {
         Sucursal nuevo = sucursalService.agregarSucursal(sucursal);
         if (nuevo != null) {
@@ -58,7 +76,10 @@ public class SucursalController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteSucursal(@PathVariable String id) {
+    @Operation(summary = "BORRAR sucursal", description = "Borra la sucursal mediante su ID")
+    @ApiResponse(responseCode = "200", description = "Sucursal borrada exitosamente")
+    @ApiResponse(responseCode = "400", description = "Error al borrar la sucursal")
+    public ResponseEntity<Void> deleteSucursal(@Parameter(description = "ID a buscar")@PathVariable String id) {
         boolean res = sucursalService.borrarSucursalPorCodigoSucursal(id);
         if (res) {
             return new ResponseEntity<>(HttpStatus.OK);
@@ -68,7 +89,10 @@ public class SucursalController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Sucursal> updateSucursal(@PathVariable String id, @RequestBody @Valid Sucursal nuevo) {
+    @Operation(summary = "ACTUALIZAR sucursal", description = "Actualiza la sucursal mediante su ID")
+    @ApiResponse(responseCode = "200", description = "Sucursal actualizada exitosamente")
+    @ApiResponse(responseCode = "400", description = "Error al actualizar la sucursal")
+    public ResponseEntity<Sucursal> updateSucursal(@Parameter(description = "ID a buscar")@PathVariable String id, @RequestBody @Valid Sucursal nuevo) {
         Sucursal actualizado = sucursalService.actualizarSucursal(nuevo, id);
         if (actualizado != null) {
             return new ResponseEntity<>(actualizado, HttpStatus.OK);
--- usuario/src/test/java/com/gym/Usuario/UsuarioApplicationTests.java
+++ usuario/src/test/java/com/gym/Usuario/UsuarioApplicationTests.java
@@ -1,7 +1,14 @@
 package com.gym.Usuario;
 
+import com.gym.Usuario.model.Usuario;
+import com.gym.Usuario.service.UsuarioService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class UsuarioApplicationTests {
@@ -9,5 +16,4 @@ class UsuarioApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
