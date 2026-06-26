1a2c54b7533fd38c0310246fb39f6b75e398bf3b
Mativs <m.franke.u@gmail.com>
2026-06-26 14:44:28 -0400

    Se agregan los Application properties y test de ms Asistencia, Empleado, Sucursal y Usuario

--- Asistencia/src/test/java/com/gym/Asistencia/AsistenciaApplicationTests.java
+++ Asistencia/src/test/java/com/gym/Asistencia/AsistenciaApplicationTests.java
@@ -1,13 +1,41 @@
 package com.gym.Asistencia;
 
+import com.gym.Asistencia.Model.Asistencia;
+import com.gym.Asistencia.Service.AsistenciaService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class AsistenciaApplicationTests {
+	@Autowired
+
+    AsistenciaService asistenciaService;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	@DisplayName("Insertar nueva Asistencia exitosamente")
+	void testAgregarAsistencia_Exito() {
+		Asistencia nueva = new Asistencia();
+		nueva.setIdAsistencia("AS0999");
+		nueva.setFechaAsistencia(java.time.LocalDate.now());
+		nueva.setHoraEntrada(java.time.LocalTime.of(10, 30));
+		nueva.setHoraSalida(java.time.LocalTime.of(12, 00));
+
+		nueva.setIdUsuario("US0001");
+		nueva.setIdSucursal("SUC001");
+
+		Asistencia resultado = asistenciaService.addAsistencia(nueva);
+
+		assertNotNull(resultado);
+
 	}
 
 }
--- Sucursal/src/test/java/com/gym/Sucursal/SucursalApplicationTests.java
+++ Sucursal/src/test/java/com/gym/Sucursal/SucursalApplicationTests.java
@@ -1,13 +1,41 @@
 package com.gym.Sucursal;
 
+import com.gym.Sucursal.Model.Sucursal;
+import com.gym.Sucursal.Service.SucursalService;
+import com.netflix.discovery.converters.Auto;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SucursalApplicationTests {
 
+	@Autowired
+	SucursalService sucursalService;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	@DisplayName("Insertar nueva Sucursal exitosamente")
+	void testAgregarSucursal_Exito() {
+		Sucursal nueva = new Sucursal();
+		nueva.setIdSucursal("SUC999");
+		nueva.setNombreSucursal("GYM Providencia");
+		nueva.setDireccionSucursal("Avenida Siempre Viva 123");
+		nueva.setComunaSucursal("Providencia");
+		nueva.setTelefonoSucursal(912345678L);
+		nueva.setCapacidadSucursal(150);
+
+		Sucursal resultado = sucursalService.agregarSucursal(nueva);
+
+		assertNotNull(resultado);
+
 	}
 
 }
