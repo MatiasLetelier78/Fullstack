d7057997e931c82561ab00af958b4013271320c0
fernunda <fer.viveros@duocuc.cl>
2026-06-26 01:05:31 -0400

    avance?

--- clase/src/main/java/com/Gym/clase/Controller/ClaseController.java
+++ clase/src/main/java/com/Gym/clase/Controller/ClaseController.java
@@ -2,6 +2,10 @@ package com.Gym.clase.Controller;
 
 import com.Gym.clase.Model.Clase;
 import com.Gym.clase.Service.ClaseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,12 +16,16 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/clases")
+@Tag(name = "Clase", description = "operaciones relacionadas con las clases del gimnasio")
 public class ClaseController {
 
     @Autowired
     private ClaseService claseService;
 
     @GetMapping("")
+    @Operation(summary = "Listar todas las clases", description = "Retorna una lista con todas las clases disponibles")
+    @ApiResponse(responseCode = "200", description = "Clases encontradas")
+    @ApiResponse(responseCode = "204", description = "No hay clases registradas")
     public ResponseEntity<List<Clase>> getAllClases() {
         List<Clase> listado = claseService.listarClases();
         if (listado.isEmpty()) {
@@ -28,6 +36,9 @@ public class ClaseController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar clase por ID", description = "Permite localizar una clase utilizando el ID.")
+    @ApiResponse(responseCode = "200", description = "Usuario encontrado")
+    @ApiResponse(responseCode = "404", description = "Usuario no encontrado")
     public ResponseEntity<Clase> getClaseById(@PathVariable String id) {
         Clase buscada = claseService.findByIdClase(id);
         if (buscada != null) {
@@ -39,6 +50,9 @@ public class ClaseController {
 
 
     @PostMapping("/")
+    @Operation(summary = "Crear nueva clase", description = "Registra una nueva clase en la base de datos validando que el ID no exista previamente.")
+    @ApiResponse(responseCode = "201", description = "clase creada exitosamente")
+    @ApiResponse(responseCode = "400", description = "Datos inválidos o la clase ya existe")
     public ResponseEntity<Clase> createClase(@RequestBody @Valid Clase clase) {
         Clase nueva = claseService.agregarClase(clase);
         if (nueva != null) {
@@ -49,6 +63,9 @@ public class ClaseController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar una clase", description = "Borra una clase del sistema utilizando su ID.")
+    @ApiResponse(responseCode = "200", description = "Usuario eliminado correctamente")
+    @ApiResponse(responseCode = "400", description = "Error al intentar eliminar el usuario (ID no existe)")
     public ResponseEntity<Void> deleteClase(@PathVariable String id) {
         boolean res = claseService.borrarClase(id);
         if (res) {
@@ -59,7 +76,10 @@ public class ClaseController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Clase> updateClase(@PathVariable String id, @RequestBody @Valid Clase nueva) {
+    @Operation(summary = "Actualizar datos de la clase", description = "Actualiza la información de una clase existente por su ID.")
+    @ApiResponse(responseCode = "200", description = "clase actualizada exitosamente")
+    @ApiResponse(responseCode = "400", description = "la clase no existe o los datos enviados no son válidos")
+    public ResponseEntity<Clase> updateClase(@Parameter(description = "ID de clase a consultar") @PathVariable String id, @RequestBody @Valid Clase nueva) {
         Clase actualizada = claseService.actualizarClase(id, nueva);
         if (actualizada != null) {
             return new ResponseEntity<>(actualizada, HttpStatus.OK);
--- rutina/src/main/java/com/Gym/rutina/Controller/RutinaController.java
+++ rutina/src/main/java/com/Gym/rutina/Controller/RutinaController.java
@@ -2,6 +2,10 @@ package com.Gym.rutina.Controller;
 
 import com.Gym.rutina.Model.Rutina;
 import com.Gym.rutina.Service.RutinaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,12 +16,18 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/rutinas")
+@Tag(name="API Rutina",description = "API para la gestión de rutinas")
+
 public class RutinaController {
 
     @Autowired
     private RutinaService rutinaService;
 
     @GetMapping("")
+    @Operation(summary = "Obtener todas las rutinas", description = "Endpoint permite consultar todas las rutinas")
+    @ApiResponse(responseCode = "200", description = "Consulta exitosa, se entrega lista de rutinas")
+    @ApiResponse(responseCode = "204", description = "consulta exitosa, pero no se encontraron rutinas")
+
     public ResponseEntity<List<Rutina>> getAllRutinas() {
         List<Rutina> listado = rutinaService.listarRutinas();
         if (listado.isEmpty()) {
@@ -28,7 +38,11 @@ public class RutinaController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Rutina> getRutinaById(@PathVariable String id) {
+    @Operation(summary = "Buscar rutina por ID", description = "Retorna los datos de una rutina en específico según su ID interno.")
+    @ApiResponse(responseCode = "200", description = "rutina encontrada")
+    @ApiResponse(responseCode = "404", description = "rutina no encontrada")
+
+    public ResponseEntity<Rutina> getRutinaById(@Parameter(description = "ID de la rutina a consultar")@PathVariable String id) {
         Rutina buscada = rutinaService.buscarPorId(id);
         if (buscada != null) {
             return new ResponseEntity<>(buscada, HttpStatus.OK);
@@ -38,6 +52,10 @@ public class RutinaController {
     }
 
     @GetMapping("/usuario/{idUsuario}")
+    @Operation(summary = "Buscar rutina con Usuario", description = "Permite localizar una rutina utilizando el usuario.")
+    @ApiResponse(responseCode = "200", description = "rutina encontrada")
+    @ApiResponse(responseCode = "404", description = "rutina no encontrada")
+
     public ResponseEntity<List<Rutina>> getRutinasByUsuario(@PathVariable String idUsuario) {
         List<Rutina> listado = rutinaService.buscarPorUsuario(idUsuario);
         if (listado.isEmpty()) {
@@ -48,6 +66,10 @@ public class RutinaController {
     }
 
     @PostMapping("/")
+    @Operation(summary = "Crear nuevo usuario", description = "Registra un nuevo usuario en la base de datos validando que el ID no exista previamente.")
+    @ApiResponse(responseCode = "201", description = "Usuario creado exitosamente")
+    @ApiResponse(responseCode = "400", description = "Datos inválidos o el usuario ya existe")
+
     public ResponseEntity<Rutina> createRutina(@RequestBody @Valid Rutina rutina) {
         Rutina nueva = rutinaService.agregarRutina(rutina);
         if (nueva != null) {
@@ -58,6 +80,10 @@ public class RutinaController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Eliminar una rutina", description = "Borra una rutina del sistema utilizando su ID.")
+    @ApiResponse(responseCode = "200", description = "rutina eliminada correctamente")
+    @ApiResponse(responseCode = "400", description = "Error al intentar eliminar la rutina (ID no existe)")
+
     public ResponseEntity<Void> deleteRutina(@PathVariable String id) {
         boolean res = rutinaService.borrarRutina(id);
         if (res) {
@@ -68,6 +94,10 @@ public class RutinaController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Actualizar datos de la rutina", description = "Actualiza la información de una rutina existente por su ID.")
+    @ApiResponse(responseCode = "200", description = "rutina actualizada exitosamente")
+    @ApiResponse(responseCode = "400", description = "la rutina no existe o los datos enviados no son válidos")
+
     public ResponseEntity<Rutina> updateRutina(@PathVariable String id, @RequestBody @Valid Rutina nueva) {
         Rutina actualizada = rutinaService.actualizarRutina(id, nueva);
         if (actualizada != null) {
